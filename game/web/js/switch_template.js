32a10cbeb64a897a6e79d3cfecfee26c59ead80d
qiaoniYue <510348616@qq.com>
2019-05-14 14:48:17 +0800

    fasf

--- game/web/js/switch_template.js
+++ game/web/js/switch_template.js
@@ -1,11 +1,11 @@
-$("#powFiveFlash #powButPrev img").click(function(){
+$("#powFiveFlash #powButNext a").click(function(){
     $self = $('#powAlsoList');
     $self.stop().animate({"margin-left" : -242 +"px"},800 , function(){
         $self.css({"margin-left":"0px"}).find("li:first").appendTo($self);
     });
 });
 
-$("#powFiveFlash #powButNext img").click(function(){
+$("#powFiveFlash #powButPrev a").click(function(){
     $self = $('#powAlsoList');
     $self.css({"margin-left" : -242 +"px"}).find("li:last").prependTo($self);
     $self.stop().animate({"margin-left" : "0px"},800 , function(){});
